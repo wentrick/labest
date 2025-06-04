306b5e64a80d368e631b0e60dcaefb5124008599
Desktop_DWF <davi.w.feijo@gmail.com>
2025-06-03 21:44:40 -0300

    melhorias graficas

--- labest_2025/relatorio/relatorio_ubs/relatorio_temas_2.R
+++ labest_2025/relatorio/relatorio_ubs/relatorio_temas_2.R
@@ -1,32 +1,9 @@
 # 1. Carregar pacotes
 pacman::p_load(tidyverse, stringr, readxl, scales, janitor, openxlsx)
 
-#Esse codigo ele junta uma base feita a partir de outra fonte de dados como forma de estimar o porte, porem a coluna pop ja tem info da populacao tambem 
-#entao eu vou criar outro codigo que vai gerar o porte a partir dessa coluna (Vou fazer isso pois vai que precisamos usar esse)
+# Esse codigo calcula o porte direto a partir da coluna populacao da pop_cadastrada_2023
 
-# 1. Lê e trata o arquivo de porte
-municipio_porte <- read_excel("labest_2025/data/POP2022_Municipios.xls", skip = 1) %>%
-  slice(-1) %>%                    
-  rename(population = POPULAÇÃO) %>%
-  mutate(
-    population = as.numeric(population),
-    `NOME DO MUNICÍPIO` = str_to_upper(`NOME DO MUNICÍPIO`),
-    porte_ibge = case_when(
-      population <= 5000   ~ "Pequeno I (até 5.000 hab.)",
-      population <= 10000  ~ "Pequeno II (5.001–10.000 hab.)",
-      population <= 20000  ~ "Médio I (10.001–20.000 hab.)",
-      population <= 50000  ~ "Médio II (20.001–50.000 hab.)",
-      population <= 100000 ~ "Grande I (50.001–100.000 hab.)",
-      TRUE                 ~ "Grande II (> 100.000 hab.)"
-    ),
-    Porte = str_extract(porte_ibge, "^[^()]+I{1,2}")
-  ) %>%
-  select(uf = UF,
-    municipio = `NOME DO MUNICÍPIO`,  # ajusta para casar exatamente com o nome em 'mun'
-    Porte
-  )
-
-# 2. Gera 'mun' conforme você já vinha fazendo
+# 2. Define regiões
 regioes <- list(
   Norte          = c("AC","AP","AM","PA","RO","RR","TO"),
   Nordeste       = c("MA","PI","CE","RN","PB","PE","AL","SE","BA"),
@@ -35,6 +12,7 @@ regioes <- list(
   `Centro-Oeste` = c("MT","MS","GO","DF")
 )
 
+# 3. Lê os dados
 temas <- read_excel("labest_2025/data/UBS - Atencao Primaria/temas_municipio_2023.xlsx") %>%
   clean_names() %>%
   select(uf, ibge, municipio,
@@ -44,10 +22,14 @@ temas <- read_excel("labest_2025/data/UBS - Atencao Primaria/temas_municipio_202
 
 pop <- read_excel("labest_2025/data/UBS - Atencao Primaria/pop_cadastrada_2023_denominador_temas_e_praticas.xls") %>%
   clean_names() %>%
-  rename(pop_total = qt_total_de_cadastros_limitado_pela_populacao_ibge) %>%
-  mutate(pop_total = as.numeric(pop_total)) %>%
-  drop_na(pop_total)
+  rename(
+    pop_total = qt_total_de_cadastros_limitado_pela_populacao_ibge,
+    population = populacao  # <- ajustar o nome conforme está no seu arquivo
+  ) %>%
+  mutate(across(c(pop_total, population), as.numeric)) %>%
+  drop_na(pop_total, population)
 
+# 4. Junta os dados e calcula indicadores
 mun <- temas %>%
   left_join(pop, by = "ibge") %>%
   rename(uf = uf.x, municipio = municipio.x) %>%
@@ -74,22 +56,27 @@ mun <- temas %>%
       uf %in% regioes$Sul            ~ "Sul",
       uf %in% regioes$`Centro-Oeste` ~ "Centro-Oeste",
       TRUE                            ~ NA_character_
+    ),
+    Porte = case_when(
+      population <= 5000   ~ "Pequeno I (até 5.000 hab.)",
+      population <= 10000  ~ "Pequeno II (5.001–10.000 hab.)",
+      population <= 20000  ~ "Médio I (10.001–20.000 hab.)",
+      population <= 50000  ~ "Médio II (20.001–50.000 hab.)",
+      population <= 100000 ~ "Grande I (50.001–100.000 hab.)",
+      TRUE                 ~ "Grande II (> 100.000 hab.)"
     )
   ) %>%
-  # 3. JOIN final pelo nome do município para trazer a coluna Porte
-  left_join(municipio_porte, by = c("municipio","uf")) %>%
+  mutate(Porte_simplificado = str_extract(Porte, "^[^()]+I{1,2}")) %>%
   select(
-    regiao, uf, municipio, ibge, pop_total, Porte,saude_bucal, alimentacao_saudavel,
-    saude_sexual_e_reprodutiva, saude_mental, autocuidado_de_pessoas_com_doe,total_temas,
+    regiao, uf, municipio, ibge, population, pop_total, Porte,Porte_simplificado,
+    saude_bucal, alimentacao_saudavel,
+    saude_sexual_e_reprodutiva, saude_mental,
+    autocuidado_de_pessoas_com_doe, total_temas,
     starts_with("ind_")
   )
-
-# Agora 'mun' contém também a coluna 'Porte', associada por nome de município.
 
 
 # Usando openxlsx para salvar em excel
 write.xlsx(mun, file = "labest_2025/relatorio/relatorio_ubs/taxas_temas_municipio.xlsx", sheetName = "mun", rowNames = FALSE)
-
-
 
 
